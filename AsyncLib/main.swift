eae898901d154635f3a1507c0b3f8a4d8efa1821
Chris Montrois <cmontrois@quickleft.com>
2014-11-12 13:45:53 -0700

    Initial Commit

---
+++ AsyncLib/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AsyncLib
+//
+//  Created by Chris Montrois on 11/12/14.
+//  Copyright (c) 2014 bigevilrobot. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
