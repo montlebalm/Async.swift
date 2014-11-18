02ebc6277f2b2ba52f47eb3c35e9094b0e9c45fa
Chris Montrois <cmontrois@quickleft.com>
2014-11-18 11:05:17 -0700

    Making helper fns private class fns

--- Async/Async.swift
+++ Async/Async.swift
@@ -8,53 +8,8 @@
 
 import Foundation
 
-func _map<I, O>(tasks: [I], complete: (NSError?, [O]) -> (), iterator: (I, (NSError?, O) -> ()) -> ()) {
-  var temp: [(Int, O)] = []
-  
-  let next = { (err: NSError?, result: O, index: Int) -> () in
-    if err != nil {
-      complete(err, [])
-    } else {
-      temp.append((index, result))
-      
-      if temp.count == tasks.count {
-        let sortedTemp = temp.sorted { $0.0 < $1.0 }
-        let results = sortedTemp.map { $0.1 }
-        complete(nil, results)
-      }
-    }
-  }
-  
-  for i in 0..<tasks.count {
-    iterator(tasks[i]) { err, result in
-      next(err, result, i)
-    }
-  }
-}
-
-func _each<I>(tasks: [I], complete: (NSError?) -> (), iterator: (I, (NSError?) -> ()) -> ()) {
-  var remaining = tasks.count
-  let next = { (err: NSError?, index: Int) -> () in
-    if err != nil {
-      complete(err)
-    } else {
-      remaining -= 1
-
-      if remaining == 0 {
-        complete(nil)
-      }
-    }
-  }
-  
-  for i in 0..<tasks.count {
-    iterator(tasks[i]) { err in
-      next(err, i)
-    }
-  }
-}
-
 class Async {
-
+  
   class func each<I>(items: [I], transform: (I, (NSError?) -> ()) -> (), complete: (NSError?) -> ()) {
     _each(items, complete) { item, next in
       dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
@@ -62,7 +17,7 @@ class Async {
       }
     }
   }
-
+  
   class func map<I, O>(items: [I], transform: (I, (NSError?, O) -> ()) -> (), complete: (NSError?, [O]) -> ()) {
     _map(items, complete, transform)
   }
@@ -78,6 +33,50 @@ class Async {
   class func series<O>(tasks: [((NSError?, O) -> ()) -> ()], complete: (NSError?, [O]) -> ()) {
     _map(tasks, complete) { task, next in
       task(next)
+    }
+  }
+  
+  private class func _map<I, O>(tasks: [I], complete: (NSError?, [O]) -> (), iterator: (I, (NSError?, O) -> ()) -> ()) {
+    var temp: [(Int, O)] = []
+    
+    let next = { (err: NSError?, result: O, index: Int) -> () in
+      if err != nil {
+        complete(err, [])
+      } else {
+        temp.append((index, result))
+        
+        if temp.count == tasks.count {
+          // Put the results in the original order
+          let sortedTemp = temp.sorted { $0.0 < $1.0 }
+          let results = sortedTemp.map { $0.1 }
+          complete(nil, results)
+        }
+      }
+    }
+    
+    for i in 0..<tasks.count {
+      iterator(tasks[i]) { err, result in
+        next(err, result, i)
+      }
+    }
+  }
+  
+  private class func _each<I>(tasks: [I], complete: (NSError?) -> (), iterator: (I, (NSError?) -> ()) -> ()) {
+    var remaining = tasks.count
+    let next = { (err: NSError?) -> () in
+      if err != nil {
+        complete(err)
+      } else {
+        remaining -= 1
+        
+        if remaining == 0 {
+          complete(nil)
+        }
+      }
+    }
+
+    for task in tasks {
+      iterator(task, next)
     }
   }
   
