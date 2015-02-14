6982d779af46d752222bc33761ea586f08fcec7d
Chris Montrois <cmontrois@quickleft.com>
2015-02-13 21:48:22 -0700

    Cleaning up tests

--- AsyncTests/WaterfallTests.swift
+++ AsyncTests/WaterfallTests.swift
@@ -11,31 +11,48 @@ import XCTest
 class WaterfallTests: XCTestCase {
 
   func testPreservesOrder() {
-    Async.waterfall([slow(0), fast(1)]) { err, results in
-      XCTAssertEqual(results, [0, 1])
+    func fast(results: [String], callback: (NSError?, String) -> ()) {
+      callback(nil, "fast")
+    }
+
+    func slow(results: [String], callback: (NSError?, String) -> ()) {
+      sleep(1)
+      callback(nil, "slow")
+    }
+
+    Async.waterfall([slow, fast]) { err, results in
+      XCTAssertEqual(results, ["slow", "fast"])
+    }
+  }
+
+  func testRunsInSeries() {
+    var completedOrder: [String] = []
+
+    func fast(results: [Int], callback: (NSError?, Int) -> ()) {
+      completedOrder.append("fast")
+      callback(nil, 0)
+    }
+
+    func slow(results: [Int], callback: (NSError?, Int) -> ()) {
+      sleep(1)
+      completedOrder.append("slow")
+      callback(nil, 0)
+    }
+
+    Async.waterfall([slow, fast]) { err, results in
+      XCTAssertEqual(completedOrder, ["slow", "fast"])
     }
   }
 
   func testResultsPassedAlong() {
-    Async.waterfall([sumPlusOne, sumPlusOne, sumPlusOne]) { err, results in
-      XCTAssertEqual(results, [1, 2, 4])
+    func sumPlusOne(results: [Int], callback: (NSError?, Int) -> ()) {
+      let sum = results.reduce(0, combine: +)
+      callback(nil, sum + 1)
     }
-  }
 
-  // Helpers
-
-  private func sumPlusOne(results: [Int], callback: (NSError?, Int) -> ()) {
-    let sum = results.reduce(0) { $0 + $1 }
-    callback(nil, sum + 1)
-  }
-
-  private func fast(order: Int)(results: [Int], callback: (NSError?, Int) -> ()) {
-    callback(nil, order)
-  }
-
-  private func slow(order: Int)(results: [Int], callback: (NSError?, Int) -> ()) {
-    sleep(1)
-    callback(nil, order)
+    Async.waterfall([sumPlusOne, sumPlusOne]) { err, results in
+      XCTAssertEqual(results, [1, 2])
+    }
   }
 
 }
