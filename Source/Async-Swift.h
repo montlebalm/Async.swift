8dbd61db5928c6015cadbf7edd6f1d75ada73808
Chris Montrois <cmontrois@quickleft.com>
2015-02-15 16:34:50 -0700

    Rearranging files

---
+++ Source/Async-Swift.h
@@ -0,0 +1,13 @@
+//
+//  Async.h
+//  Async
+//
+//  Created by Chris Montrois on 2/15/15.
+//  Copyright (c) 2015 bigevilrobot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Async : NSObject
+
+@end
